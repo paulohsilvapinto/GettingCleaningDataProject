a99148e2c982b82efb21eaf63e4c0923dc8a9861
Paulo Henrique Silva Pinto <paulohsilvapinto@gmail.com>
2015-02-13 20:19:41 -0200

    Second Version. Fully recoded

--- run_analysis.R
+++ run_analysis.R
@@ -1,63 +1,103 @@
-samsumg_file_zip <- "getdata-projectfiles-UCI HAR Dataset.zip"
-samsumg_file_unzip <-"UCI HAR Dataset"
-samsumg_file_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+#------------------------------------------
+# Coursera - Getting and Cleaning Data
+# Johns Hopkins University
+# Course Project
+#------------------------------------------
+# Define Files variables
+#------------------------------------------
 
-#Verifies if file exists. If it doesnt, then download it
-if(!file.exists(samsumg_file_unzip)){
+samsumg_file_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+samsumg_zip_file <- "getdata-projectfiles-UCI HAR Dataset.zip"
+samsumg_unzip_file <-"UCI HAR Dataset"
+features_file <- "UCI HAR Dataset/features.txt"
+activity_file <- "UCI HAR Dataset/activity_labels.txt"
+subject_train_file <- "UCI HAR Dataset/train/subject_train.txt"
+x_train_file <- "UCI HAR Dataset/train/X_train.txt"
+y_train_file <- "UCI HAR Dataset/train/Y_train.txt"
+subject_test_file <- "UCI HAR Dataset/test/subject_test.txt"
+x_test_file <- "UCI HAR Dataset/test/X_test.txt"
+y_test_file <- "UCI HAR Dataset/test/Y_test.txt"
+
+#------------------------------------------
+# 0.Verifies if file exists. If it doesnt, then download/unzip it
+#------------------------------------------
+
+if(!file.exists(samsumg_unzip_file)){
         
-        if(!file.exists(samsumg_file_zip)){
+        if(!file.exists(samsumg_zip_file)){
                 download.file(samsumg_file_url, destfile = samsumg_file)
         }
         #unzip the zip data file       
-        unzip(samsumg_file_zip)
+        unzip(samsumg_zip_file)
 }
 
+#------------------------------------------
+# 1. Merges the training and the test sets to create one data set.
+#------------------------------------------
+
+train_df <- cbind(a=read.table(subject_train_file)[,1],
+                  b=read.table(y_train_file)[,1],
+                  c=read.table(x_train_file))
+
+test_df <- cbind(a=read.table(subject_test_file)[,1],
+                 b=read.table(y_test_file)[,1],
+                 c=read.table(x_test_file))
+
+merged_df <- rbind(train_df, test_df)
 
 
-#Create data.frames with descriptions
-features_description_df <- read.table("UCI HAR Dataset/features.txt")
-activities_description_df <- read.table("UCI HAR Dataset/activity_labels.txt")
 
-#select the columns indexes that corresponds to mean or std and then sort them.
-#The *Mean* columns wasnt selected cause its an angle, not the mean.
-mean_columns <- grep("mean", features_description_df[,2])
-std_columns <- grep("std", features_description_df[,2])
-measures_columns <- sort(c(mean_columns, std_columns))
+#------------------------------------------
+# 2. Extracts only the measurements on the mean and standard deviation for each 
+#measurement. 
+#------------------------------------------
 
-#clear unused variables
-rm(list=c("mean_columns", "std_columns"))
+features_description_df <- read.table(features_file)
 
+measures_columns <- sort(c(grep("mean", features_description_df[,2]),
+                      grep("std", features_description_df[,2])))
 
-#Create train data.frame
-subject_train_df <- read.table("UCI HAR Dataset/train/subject_train.txt")[,1]
-activities_train_df <- factor(readLines(file("UCI HAR Dataset/train/Y_train.txt")),
-                        levels=activities_description_df[,1],
-                        labels=activities_description_df[,2])
-measures_train_df <- read.table("UCI HAR Dataset/train/X_train.txt")[,measures_columns]
-colnames(measures_train_df) <- features_description_df[measures_columns,2]
+df_columns=c(1,2, measures_columns+2)
 
-train <- cbind(subject=subject_train_df,
-               activity=activities_train_df,
-               measures_train_df)
-
-#Create test data.frame
-subject_test_df <- read.table("UCI HAR Dataset/test/subject_test.txt")[,1]
-activities_test_df <- factor(readLines(file("UCI HAR Dataset/test/Y_test.txt"))
-                              ,levels=activities_description_df[,1]
-                              ,labels=activities_description_df[,2])
-measures_test_df <- read.table("UCI HAR Dataset/test/X_test.txt")[,measures_columns]
-colnames(measures_test_df) <- features_description_df[measures_columns,2]
-
-test <- cbind(subject=subject_test_df,
-               activity=activities_test_df,
-               measures_test_df)
-
-#Create data.frame merging train_df and test_df
-merged_df <- rbind(train, test)
+merged_df <- merged_df[,df_columns]
 
 
-tiny_df <- aggregate(merged_df[,3:81], merged_df[,c("subject", "activity")], function(x) mean(x))
+
+#------------------------------------------
+# 3. Uses descriptive activity names to name the activities in the data set
+#------------------------------------------
+
+activities_description_df <- read.table(activity_file)
+
+merged_df[,2] <- factor(merged_df[,2],
+                        levels = activities_description_df[,1],
+                        labels = activities_description_df[,2])
+
+
+
+#------------------------------------------
+# 4. Appropriately labels the data set with descriptive variable names.
+#------------------------------------------
+
+colnames(merged_df) <- c("subject"
+                         ,"activity"
+                         , as.character(features_description_df[measures_columns,2]))
+
+#------------------------------------------
+# 5. From the data set in step 4, creates a second, independent tidy data set with 
+#    the average of each variable for each activity and each subject.
+#-----------------------------------------
+
+ncolumns <- ncol(merged_df)
+
+tiny_df <- aggregate(merged_df[,3:ncolumns], merged_df[,1:2], function(x) mean(x))
 tiny_df <- tiny_df[order(tiny_df[,1], tiny_df[,2]),]
-write.table(tiny_df, file="projeto.txt", row.names = FALSE)
 
-rm(list=setdiff(ls(), "tiny_df"))
+colnames(tiny_df) <- c("subject"
+                       ,"activity"
+                       ,paste(colnames(tiny_df[3:ncolumns]), "_average", sep = ""))
+
+write.table(tiny_df, file="tiny_data.txt", row.names = FALSE)
+
+# Remove all variables except merged_df and tiny_df
+rm(list=setdiff(ls(), c("merged_df", "tiny_df")))
